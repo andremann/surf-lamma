92f93169d7e68bc710ea1ab9287e729f20134ae7
Andrea Mannocci <andremann@libero.it>
2020-10-16 10:17:03 +0200

    added model switch

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 var app = new Vue({
     el: '#lamma-surf',
     data: {
-      wave: "http://www.lamma.rete.toscana.it/models/ww3hr/last/swh.B.",
-      wind: "http://www.lamma.rete.toscana.it/models/ww3hr/last/wind10.B.",
-      current: 1,
-      flag: true
+        base: "http://www.lamma.rete.toscana.it/models/ww3",
+        wave: "/last/swh.",
+        wind: "/last/wind10.",
+        current: 1,
+        flag: true,
+        model: 'lr',
+        area: 'M'
     },
     methods: {
-      prev: function() {
-        this.current = this.current - 1;
-  
-      },
-      next: function() {
-        this.current = this.current + 1;
-  
-      },
-      swap: function() {
-        this.flag = !this.flag;
-  
-      },
+        prev: function () {
+            this.current = this.current - 1;
+        },
+        next: function () {
+            this.current = this.current + 1;
+        },
+        swap: function () {
+            this.flag = !this.flag;
+        },
+        swap_model: function () {
+            if (this.model == 'lr') {
+                this.model = 'hr';
+                this.area = 'B';
+            } else if (this.model == 'hr') {
+                this.model = 'lr';
+                this.area = 'M';
+            }
+        }
     }
-  })
-  
+})
